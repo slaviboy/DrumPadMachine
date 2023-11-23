ac83e594dc4bf80adcee74f96124ed047ada93b6
slaviboy <slavi94slavi94@gmail.com>
2023-11-23 20:53:29 +0200

    Add splash screen and set full screen theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,10 +47,11 @@ android {
 }
 
 dependencies {
+    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
--- app/src/main/java/com/slaviboy/drumpadmachine/MainActivity.kt
+++ app/src/main/java/com/slaviboy/drumpadmachine/MainActivity.kt
@@ -3,12 +3,33 @@ package com.slaviboy.drumpadmachine
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .background(
+                        brush = Brush.verticalGradient(
+                            listOf(
+                                Color(0xFF2F2F40),
+                                Color(0xFF161529)
+                            )
+                        )
+                    )
+            ) {
 
+            }
         }
     }
 }
