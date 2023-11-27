4da1b505932e83559185bf354e1b5f453e056667
slaviboy <slavi94slavi94@gmail.com>
2023-11-27 21:43:49 +0200

    Update CMake

--- audio/src/main/cpp/audio.cpp
+++ audio/src/main/cpp/audio.cpp
@@ -1,12 +1,21 @@
 #include <jni.h>
 #include <string>
-
+#include <oboe/Oboe.h>
 #include <flowgraph/resampler/MultiChannelResampler.h>
 #include <player/OneShotSampleSource.h>
+#include "player/SimpleMultiPlayer.h"
+
+//static iolib::SimpleMultiPlayer sDTPlayer;
+static oboe::resampler::MultiChannelResampler *a = oboe::resampler::MultiChannelResampler::make(
+        1,
+        2,
+        3,
+        oboe::resampler::MultiChannelResampler::Quality::Medium
+);
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_slaviboy_audio_NativeLib_stringFromJNI(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
