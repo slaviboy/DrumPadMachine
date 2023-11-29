8e9fbc0b03d48c92b822f69c0562fdd60a7adb80
slaviboy <slavi94slavi94@gmail.com>
2023-11-29 18:47:05 +0200

    Remove fileTree dependencies

--- parselib/build.gradle.kts
+++ parselib/build.gradle.kts
@@ -1,5 +1,4 @@
 dependencies {
-    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation(*ApplicationDependencies.dependenciesLibraryJNI)
     project(":oboe")
 }
